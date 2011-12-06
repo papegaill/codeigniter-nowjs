84eb909da4a7a0e3508c957289332eadc3eb8265
George Samper <george.samper@gmail.com>
2011-12-06 20:01:47 +0100

    Update readme

--- application/libraries/MY_Session.php
+++ application/libraries/MY_Session.php
@@ -5,7 +5,7 @@ class MY_Session extends CI_Session {
 	public function __construct()
 	{
 		$CI = get_instance();
-
+		//Extend the session class so that it doesnt create a session cookie and row in db if the request comes from commandline
 		if ($CI->input->is_cli_request())
 		{
 			return;
